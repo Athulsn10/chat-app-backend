a4bf4ab82863cccda2b0a08af032da313f246e7e
Athulsn10 <athulnair10@outlook.com>
2023-11-22 21:08:21 +0530

    mongoose,userschema,chatschema,messageschema

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const { chats } = require('./Data/data');
+
+const app = express()
+console.log(chats);
+dotenv.config();
+app.get('/',(req,res)=>{
+    res.send('API is running succesfully')
+});
+
+app.get('/api/chat',(req,res)=>{
+    res.send(chats)
+    
+});
+
+app.get('/api/chat/:id',(req,res)=>{
+    // console.log(req.params.id);
+    const singleChat = chats.find((c)=>c._id === req.params.id);
+    res.send(singleChat);
+})
+
+app.get('/aoi/chat/:id',(req,res)=>{
+    console.log(req.params.id);
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(5000,console.log(`Server Started on port ${PORT} `))
