e8046875e0ddb109b5b90005ff759bf8cceb84c9
Athulsn10 <athulnair10@outlook.com>
2023-11-25 00:31:35 +0530

    user authentication

---
+++ controller/userControllers.js
@@ -0,0 +1,54 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel');
+const { response } = require('express');
+const generateToken = require('../config/generateToken')
+
+const registerUser = asyncHandler(async (req,res)=>{
+    const {name,email,password,pic} = req.body;
+    if(!name || !email || !password){
+        response.status(400);
+        throw new Error('please Enter All Fields')
+    }
+    const userExists = await User.findOne({email})
+    if(userExists){
+        response.status(400);
+        throw new Error('User Already Exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        pic
+    });
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            pic: user.pic,
+            token: generateToken(user._id)
+        })
+    }else{
+        throw new Error("failed to create user")
+    }
+});
+
+const authUser =asyncHandler(async (req,res)=>{
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if( user && (await user.matchPassword(password))){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            pic: user.pic,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(401);
+        throw new Error("invalid credentials")
+    }
+})
+
+module.exports={registerUser,authUser}
--- models/userModel.js
+++ models/userModel.js
@@ -1,30 +1,39 @@
-// User Schema
-
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required: true
-    },
-    email:{
-        type:String,
-        required: true
-    },
-    password:{
-        type: String,
-        required:true
-    },
-    profilePic:{
-        type: String,
-        required: true,
-        default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-    }
-},
-{
-    timestamps:true
-}
-)
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
+    type: String,
+    default: 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg',
+  },
+});
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password before storing to database
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(15);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
 module.exports = User;
