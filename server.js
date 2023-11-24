fda4c8c4a5470868338d980cb6cce7e660d255e1
Athulsn10 <athulnair10@outlook.com>
2023-11-25 00:31:35 +0530

    user authentication

--- server.js
+++ server.js
@@ -2,29 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv')
 const { chats } = require('./Data/data');
 const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 
-const app = express()
-console.log(chats);
+const app = express();
+app.use(express.json());
 dotenv.config();
 connectDB()
 app.get('/',(req,res)=>{
     res.send('API is running succesfully')
 });
 
-app.get('/api/chat',(req,res)=>{
-    res.send(chats)
-    
-});
-
-app.get('/api/chat/:id',(req,res)=>{
-    // console.log(req.params.id);
-    const singleChat = chats.find((c)=>c._id === req.params.id);
-    res.send(singleChat);
-})
-
-app.get('/aoi/chat/:id',(req,res)=>{
-    console.log("id= "+ req.params.id);
-})
+app.use('/api/user',userRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(5000,console.log(`Server Started on port ${PORT} `))
